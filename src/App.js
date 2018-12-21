c9d6750919b48dd5bd2332a7e0d1b47e577d236c
alexandercannon <a.m.t.cannon@gmail.com>
2018-12-21 23:49:09 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,164 @@
+import React, { Component } from "react";
+import logo from "./logo.jpg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <span>Company Name</span>
+          <img src={logo} className="App-logo" alt="company logo" />
+          <span>Projects</span>
+          <span>Ethos</span>
+          <span>Contact</span>
+        </header>
+        <content>
+          <introduction className="introduction">
+            Hey everybody, it's Tuuuuuuuesday! I'd have to train to fight a
+            dolphin; I wouldn't just jump out there and start fighting a
+            dolphin.
+          </introduction>
+          <about className="about">
+            <skills className="skills">
+              <ul>
+                <li>DevOps</li>
+                <li>UI</li>
+                <li>UX</li>
+                <li>Service Mesh</li>
+                <li>Self-healing systems</li>
+              </ul>
+            </skills>
+            <blurb className="blurb">
+              <p>
+                No, there’s nothing I'd rather talk about right now than Peter
+                Molyneux's balls. THESE ARE SHEETS FOR FUCKING IN. This Quick
+                Look is over! It's like I'm in a techno womb. Show me a mommy
+                Mario. My forehead hasn't grown with the rest of my body. Suck
+                on it, Ron Weasley. Fuck your milk! They're basically sperm.
+                Jeff, I have a question. Why do we keep doing this? Go to hell.
+                Blocked. You're just anti-people-believing-in-things. How weird
+                is the existence of the Christmas tree farmer? I already live in
+                a world without Batman and it fucking suuuucks. Goku's like the
+                Huell Howser of anime. Fuck you, Brad Garrett! I hope you burn
+                in hell!
+              </p>
+              <a>...more</a>
+            </blurb>
+          </about>
+          <p>
+            I'm a dick, and I want to subject other people to that. You're
+            talking about sex, right? Sometimes there are cuts where you don't
+            expect them to be. I'm talking about your butthole. Can I ask you
+            why you're saying 'booty'? I'm not gonna eat a supernatural
+            anything. Yeah, you like getting it both ways, don't you? Once you
+            hit 30 you can't do shots. Good Old Raisins and Peanuts! GORP! You
+            guys never heard of GORP? I just magic'd in my pants. You are
+            history's greatest monster. I WILL CONTINUE TO CALL PAX AUSTRALIA
+            PAXA UNTIL IT FUCKING CATCHES ON. I'm going to ride my fucking
+            resentment into a fucking volcano! She's reading your mind with her
+            scary eyes. Now that wolf is gonna fuck that pig. I'm gonna no scope
+            these crops. You can't out-engineer a fucking chainsaw. You're just
+            jealous of other peoples' hot news. Take that you shitty wizard.
+            Because the Dutch are cheap. You won't know you're playing SimCity
+            once you smoke all them bath salts, Jeff. The laziest dubstep is
+            arguably the best dubstep. I always get run over from behind.
+            Alright, now I'm not gonna wash my hands, and I'm gonna take these
+            cookies. You could put a GoPro on a bear pretty easy. So this is a
+            parable for communism. Let's play some meat! You diseased piece of
+            shit. You don't have to go home, but you can't stay here. Just gonna
+            masturbate under my invisible cloak, don't mind me. You need to spit
+            on it first. It makes me angry how fucking much I enjoy this game. I
+            didn't come here to get harassed by asshole teenagers, I get enough
+            of that as it is. You are Bathroom Guy. Well, you're trying to get
+            her pregnant, right? I am the hat judge. Show me a hat and I will
+            tell you if it's a good hat or bad hat. I want a game that is
+            nothing but kicking dumpsters at enemies. This is skin-peelingly
+            bad. Being in the tub with someone else's poop, there are few things
+            that are just as sheer terrible. Hey E3, maybe stop farting for five
+            seconds. I hate Game Room so much. It's sad that I lost my leg, but
+            let's not let it go to waste. Fight-or-flight kicks in immediately
+            when you see a man shitting in his own hand. John McTiernan, master
+            of modern fucking action movies. The man made Predator, god dammit!
+            If you're gonna sit there and tell me Renny Harlin made a better
+            movie than him, you are all assholes and I don't want to work with
+            you anymore! On death, all hummingbirds explode. Oh, Dennis Leary is
+            in this for no good reason. Moral of the story here is do not make
+            any sort of accords with laser-shooting wolves. I don't believe in
+            wheels. He's making meth. You know what, Jeff? If you were a man,
+            you'd be wearing a hat right now. He's like the J.R.R. Tolkien of
+            being shitty. I hate watching you drink warm soda. It's bad for you
+            and it's bad for the soda. This is what sadomasochism will look like
+            in the future. You are looking for far too much logical consistency.
+            You're carrying around a Lego baby, Gary. Where's the real baby? No
+            one should ever put clothes on a shark. Give me access to your
+            members-only site of hot mommy Marios. I feel like I'm gonna throw
+            up, but in a good way. Everyone's so obsessed with the Link thing,
+            no one ever asks, like, is that the same Mario every time? It's easy
+            to piss off Batman. Doesn't take much. I have a problematic toilet.
+            My dick doesn't have tits. The day spa is the ultimate high-stakes
+            "is this a public space it's OK to be naked in?" scenario. Vinny,
+            you and I both, crazy Potterheads. It's like potheads, but for Harry
+            Potter. We were talking about furries and yiffing and GenCon. Did
+            you know? Babies can't eat Legos. Fact. CHINA DON'T CARE. Harry
+            Potter and the 9/11 Conspiracy. I played Ms. Pac-Man. It's pretty
+            good. I decided this weekend that it's about communism. It's 18
+            years old, Daytona USA. You could fuck it if you wanted to. Timmy,
+            you're not a converted Primal Rage arcade cabinet, Timmy! You're a
+            stupid dog! I want you to go to hell. I hate socks. BLOCKED. I am
+            the street! Gimme that horsey sauce. There's a lot of face and neck
+            stabbing, if you like to stab dudes in the face and neck. What
+            PopCap game do you think the Master Chief would like the most? You
+            would not have the time to retaliate while you're watching this man
+            shit in his own hand, preparing to fling it at you. There's too much
+            stuff inside this Hot Pocket. Seattle is actually Spanish for
+            "taco." Those penguins are shitting Pong balls. Or, snow balls. Or I
+            guess shit balls, is what they are. Get your thumb out of your ass
+            and try playing games for once, why don't ya. The Kool-Aid Man never
+            had a flamethrower. I try not to affiliate myself with Canadians.
+            Too much in your wet diaper? Let's put our frowns on upside down,
+            and eat my buddy's leg. You're saying pipes don't have rights?
+            Fuckin' helicopters are the worst! They're trying to kill you as you
+            get in them. I've got a salty mustache. I am not smart enough to
+            know what any of that shit means. Who knows what direction that poop
+            is gonna take. Explain Baby Mario. Being there, and then all of the
+            sudden, poop floats up, hits the surface. Infinite beards. The cops
+            are here for the crimes against video games you've committed, Sonic.
+            I want it on this table. Are Depends real? Are adult diapers real?
+            Fuck your box!! You're banned, you're banned, you're Nintendo
+            banned! Oh, don't front, Jeff. You were already pooping your pants
+            before we even started recording. Can we enhance this? And Space
+            Pope himself, Mr. Patrick Klepek. You people are abject porn
+            monsters. You know what's not the new sweat? Hitler. I fuckin' love
+            Game Room. What a piece of shit. What an incredible, half-assed
+            piece of garbage this is. No rights for pipes, no rights for
+            pipes... I hate Game Room. I hate it. I actively hate it. I hate
+            sitting on the couch with you and looking at these stupid games.
+            I've got milk that's older than him. I should probably throw out
+            that milk. Hit the golf button. I've been a party to many, many
+            stupid things in my life. This is by far the stupidest. I was
+            dressed up like a pizza delivery guy; brought this F-bomb instead.
+            It's dumb, but its not transcendentally dumb. Sheep are incredibly
+            muscular. Penguins are dope. Hold on to your fucking hats,
+            gentlemen. I like that this is where we're at on Game Room: not even
+            games that were good enough to be put out. I will pay good money to
+            see you as a dirty sexual predator. YO THOSE ARE FUCKING
+            DOUBLE-UNDERLINE SICK. Don't milk a hippie. This is the result of
+            trying to touch butts. Prove that video games are good. At first I
+            cringe, but I want more. Why not just say KAIJU BATTLE ROBOT JOX?
+            Can we agree that buffalo sauce on your asshole would not feel good?
+            So you want a Hot Pocket with more pocket and less hot, is that what
+            you're saying? You are a human piece of shit. I could talk about
+            Peter Molyneux's balls for a long time. Exécutable? What the fuck
+            are you saying!? It's like they'd never seen a video game before and
+            they just decided, "Oh, this is the best one, cause I've seen one."
+            This is like a fuckin' Woody Allen heist going on here. Caused all
+            this collateral damage with my F-bombs.
+          </p>
+        </content>
+      </div>
+    );
+  }
+}
+
+export default App;
